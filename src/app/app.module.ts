4b88604668516bb9a6d17f8ea865b9b7aa84fb6a
hannah.mazri <hannah.mazri@terang.asia>
2019-12-15 13:40:23 +0800

    Changed style ext to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { RateComponent } from './rate/rate.component';
+import { TncComponent } from './tnc/tnc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent,
+    RateComponent,
+    TncComponent
   ],
   imports: [
     BrowserModule,
